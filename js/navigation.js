77bd4a4b6813a803f68f0009bed42db169dcfef7
Kamil Polanski <polanski88@gmail.com>
2019-04-28 20:40:51 +0200

    Modify navigation scritpt and clases/ids for it

--- js/navigation.js
+++ js/navigation.js
@@ -1,8 +1,40 @@
-var personal=document.querySelector("#personal");
-var general=document.querySelector("#general");
+var personal = document.querySelector("#personal");
+var general = document.querySelector("#general");
+var details = document.querySelector("#details");
+var banners = document.querySelector("#banners");
+var links = document.querySelector("#links");
+var payout = document.querySelector("#payout");
+var a;
 
-personal.addEventListener('click', function(){
-    document.querySelector('.x2').style.display="none";
-    document.querySelector('.section-p_data').classList.add("display-on");
+function display() {
+    a = document.querySelectorAll('.show');
+    for (ele of a) {
+        ele.classList.remove('show');
+        ele.classList.add('hide');
+    }
+}
+
+personal.addEventListener('click', function() {
+    display();
+    document.querySelector('.section-p_data').classList.add('show');
 });
-
+general.addEventListener('click', function() {
+    display();
+    document.querySelector('.general').classList.add('show');
+});
+details.addEventListener('click', function() {
+    display();
+    document.querySelector('.details').classList.add('show');
+});
+banners.addEventListener('click', function() {
+    display();
+    document.querySelector('.banners').classList.add('show');
+});
+links.addEventListener('click', function() {
+    display();
+    document.querySelector('.link').classList.add('show');
+});
+payout.addEventListener('click', function() {
+    display();
+    document.querySelector('.payout').classList.add('show');
+});
